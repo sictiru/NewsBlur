626dbca7e833409241d5b8b3bbe7be8eaf903ff0
Andrei <dan.andrei91@gmail.com>
2022-10-25 20:57:50 -0700

    Expose next session as immutable live data

--- clients/android/NewsBlur/src/com/newsblur/viewModel/ItemListViewModel.kt
+++ clients/android/NewsBlur/src/com/newsblur/viewModel/ItemListViewModel.kt
@@ -1,14 +1,16 @@
 package com.newsblur.viewModel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.newsblur.util.Session
 
 class ItemListViewModel : ViewModel() {
 
-    val nextSession = MutableLiveData<Session?>()
+    private val _nextSession = MutableLiveData<Session>()
+    val nextSession: LiveData<Session> = _nextSession
 
     fun updateSession(session: Session) {
-        nextSession.value = session
+        _nextSession.value = session
     }
 }
